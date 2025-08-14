a32a839a034b54d22509d81064df9644254bfb18
Shubham Sao <Shubham Sao@Shubham-Sao>
2025-08-14 17:32:40 +0530

    by Shubham

--- GitDemo28/src/com/cjc/GitDemo.java
+++ GitDemo28/src/com/cjc/GitDemo.java
@@ -1,9 +1,17 @@
 package com.cjc;
 
 public class GitDemo {
-	
+
+	public void m1() {
+		System.out.println("This is m1...");
+	}
+
+	public void m2() {
+		System.out.println("this is m2...");
+	}
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("This is demo main");
 	}
 
